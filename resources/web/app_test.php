8d066320b235a18915e3664e5e037abd4e2b6f68
dantleech <dan.t.leech@gmail.com>
2013-06-23 22:38:55 +0100

    Show errors by default

--- resources/web/app_test.php
+++ resources/web/app_test.php
@@ -2,6 +2,9 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
 $kernel = include __DIR__.'/../../bootstrap/kernel_bootstrap.php';
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
