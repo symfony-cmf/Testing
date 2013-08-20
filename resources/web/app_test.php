4e72d8bd3b254a82bd6b3a8f937900a906186818
dantleech <dan.t.leech@gmail.com>
2013-08-20 21:10:42 +0100

    Quick fix for SHTB (self-hosting test bundle)  server run regression

--- resources/web/app_test.php
+++ resources/web/app_test.php
@@ -5,6 +5,11 @@ use Symfony\Component\HttpFoundation\Request;
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
+require_once __DIR__.'/../../bootstrap/bootstrap.php';
+
+// fixme: hack!!
+$env = 'phpcr';
+
 $kernel = include __DIR__.'/../../bootstrap/kernel_bootstrap.php';
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
