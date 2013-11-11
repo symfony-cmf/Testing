48ca0b7c8bf188d71dbdc58171dc9efa99849f3d
Wouter J <waldio.webdesign@gmail.com>
2013-11-11 18:58:09 +0100

    Fixed comments

--- src/Symfony/Cmf/Component/Testing/Unit/ConfigurationTestCase.php
+++ src/Symfony/Cmf/Component/Testing/Unit/ConfigurationTestCase.php
@@ -5,11 +5,49 @@ namespace Symfony\Cmf\Component\Testing\Unit;
 use Matthias\SymfonyConfigTest\PhpUnit\AbstractConfigurationTestCase;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
+/**
+ * A class to test the Configuration class of a bundle.
+ *
+ * @author Wouter J <wouter@wouterj.nl>
+ */
 abstract class ConfigurationTestCase extends AbstractConfigurationTestCase
 {
+    /**
+     * Returns the filenames to test.
+     *
+     * It should return an array with the format and its file:
+     *
+     *     Array(
+     *       [yaml] => 'path/to/fixtures/config.yml',
+     *       [xml]  => 'path/to/fixtures/config.xml',
+     *     )
+     *
+     * Supported formats are yaml, xml and php.
+     *
+     * It can also return multiple files per format to test: 
+     *
+     *     Array(
+     *       [yaml] => array('config-1.yml', 'config-2.yml')
+     *     )
+     * 
+     * @return array
+     */
     abstract protected function getFilenames();
+
+    /**
+     * Returns an array with the expected result after parsing the config.
+     *
+     * @return array
+     */
     abstract protected function getExpectedResult();
+
+    /**
+     * Returns the Extension class of the component.
+     *
+     * @return ExtensionInterface
+     */
     abstract protected function getExtension();
 
     public function testYamlConfig()
@@ -48,7 +86,7 @@ abstract class ConfigurationTestCase extends AbstractConfigurationTestCase
         }
     }
 
-    public function testXmlConfig()
+    public function testPhpConfig()
     {
         $filenames = $this->getFilenames();
 
@@ -66,16 +104,31 @@ abstract class ConfigurationTestCase extends AbstractConfigurationTestCase
         }
     }
 
+    /**
+     * Loads a XML file.
+     *
+     * @param string $file The file name
+     */
     protected function loadXmlFile($file)
     {
         return $this->doLoadFile($file, 'XmlFileLoader');
     }
 
+    /**
+     * Loads a Yaml file.
+     *
+     * @param string $file The file name
+     */
     protected function loadYamlFile($file)
     {
         return $this->doLoadFile($file, 'YamlFileLoader');
     }
 
+    /**
+     * Loads a PHP file.
+     *
+     * @param string $file The file name
+     */
     protected function loadPhpFile($file)
     {
         return $this->doLoadFile($file, 'PhpFileLoader');
