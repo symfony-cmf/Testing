1bf33b4a4ed84dea6794349d4728909b7316b020
WouterJ <waldio.webdesign@gmail.com>
2014-04-11 13:05:23 +0200

    Removed 2.2 hack

--- src/Symfony/Cmf/Component/Testing/Phpunit/DatabaseTestListener.php
+++ src/Symfony/Cmf/Component/Testing/Phpunit/DatabaseTestListener.php
@@ -26,7 +26,7 @@ class DatabaseTestListener implements \PHPUnit_Framework_TestListener
     public function __construct($processBuilder = null)
     {
         if (null === $processBuilder) {
-            $this->processBuilder = new PrefixedProcessBuilder();
+            $this->processBuilder = new ProcessBuilder();
             $phpExecutableFinder = new PhpExecutableFinder();
             $phpExecutable = $phpExecutableFinder->find();
             if (false !== $phpExecutable) {
@@ -217,54 +217,5 @@ class DatabaseTestListener implements \PHPUnit_Framework_TestListener
             }
             break;
         }
-    }
-}
-
-class PrefixedProcessBuilder extends ProcessBuilder
-{
-    private $prefixes = array();
-    private $activated = true;
-
-    public function __construct(array $arguments = array())
-    {
-        parent::__construct($arguments);
-
-        if (class_exists('Symfony\Component\HttpKernel\Kernel')) {
-            $this->activated = Kernel::VERSION_ID <= 20400;
-        } else {
-            $ref = new \ReflectionMethod(get_parent_class($this), 'setPrefix'); 
-            $doc = $ref->getDocComment();
-
-            $this->activated = false === strpos('array', $doc);
-        }
-    }
-
-    public function setPrefix($prefix)
-    {
-        $prefixes = is_array($prefix) ? $prefix : array($prefix);
-
-        if (!$this->activated) {
-            return parent::setPrefix($prefix);
-        }
-
-        foreach ($prefixes as $prefix) {
-            $this->prefixes[] = $prefix;
-        }
-        
-        return $this;
-    }
-
-    public function getProcess()
-    {
-        if (!$this->activated || 0 === count($this->prefixes)) {
-            return parent::getProcess();
-        }
-
-        $process = parent::getProcess();
-        $command = implode(' ', array_map('escapeshellarg', $this->prefixes)).' '.$process->getCommandLine();
-
-        $process->setCommandLine($command);
-
-        return $process;
     }
 }
