4b1f19e5fc729551bcfc69bdf1a90d00dd628f33
Lukas Kahwe Smith <smith@pooteeweet.org>
2014-08-21 10:23:52 +0200

    do not prepend php scripts with a php to make it easier to overwrite php with hhvm

--- bin/travis/doctrine_orm.sh
+++ bin/travis/doctrine_orm.sh
@@ -4,6 +4,6 @@ DIR_NAME=`dirname $0`
 CONSOLE_DIR=$DIR_NAME"/.."
 
 # composer install --dev
-php $CONSOLE_DIR"/console" doctrine:database:create --env=orm
-php $CONSOLE_DIR"/console" doctrine:schema:create --env=orm
-php $CONSOLE_DIR"/console" doctrine:phpcr:repository:init
+$CONSOLE_DIR"/console" doctrine:database:create --env=orm
+$CONSOLE_DIR"/console" doctrine:schema:create --env=orm
+$CONSOLE_DIR"/console" doctrine:phpcr:repository:init
