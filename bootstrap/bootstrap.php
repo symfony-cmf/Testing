a05def312a0672778fb1bed9a914f0764ce1fca0
Wouter J <waldio.webdesign@gmail.com>
2015-06-25 14:24:54 +0200

    Remove annotation registry loading

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -11,24 +11,6 @@ if (!$loader = include $vendorDir.'/autoload.php') {
         'php composer.phar install'.$nl);
 }
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-AnnotationRegistry::registerLoader(function($class) use ($loader) {
-    $loader->loadClass($class);
-
-    // this was class_exists($class, false) i.e. do not autoload.
-    // this is required so that custom annotations (e.g. TreeUiBundle
-    // annotations) are autoloaded - but they should be found by the
-    // composer loader above.
-    //
-    // This probably slows things down.
-    //
-    // @todo: Fix me.
-    return class_exists($class);
-});
-
-AnnotationRegistry::registerFile($vendorDir.'/doctrine/phpcr-odm/lib/Doctrine/ODM/PHPCR/Mapping/Annotations/DoctrineAnnotations.php');
-
 if (!defined('CMF_TEST_ROOT_DIR')) {
     define('CMF_TEST_ROOT_DIR', realpath(__DIR__.'/..'));
 }
