a0715df383f02651b6fcd7681fdf780f86d3aae3
Wouter J <waldio.webdesign@gmail.com>
2015-11-22 00:53:33 +0100

    Remove PHPCR command

--- bin/travis/doctrine_orm.sh
+++ bin/travis/doctrine_orm.sh
@@ -5,4 +5,3 @@ CONSOLE_DIR=$DIR_NAME"/.."
 # composer install --dev
 $CONSOLE_DIR"/console" doctrine:database:create --env=orm
 $CONSOLE_DIR"/console" doctrine:schema:create --env=orm
-$CONSOLE_DIR"/console" doctrine:phpcr:repository:init
