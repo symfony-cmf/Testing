c0aff04e55d8f3b201868bf254c38393e1f86c36
Maximilian Berghoff <Maximilian.berghoff@gmx.de>
2018-02-20 20:41:05 +0100

    use phpcr own repository manager and test dev version of phpcr-bundle (#174)

--- tests/Functional/BaseTestCaseTest.php
+++ tests/Functional/BaseTestCaseTest.php
@@ -11,7 +11,12 @@
 
 namespace Symfony\Cmf\Component\Testing\Tests\Functional;
 
+use Doctrine\Bundle\PHPCRBundle\Test\RepositoryManager;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Cmf\Component\Testing\Functional\DbManager\PHPCR;
 use Symfony\Cmf\Component\Testing\Tests\Fixtures\TestTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class BaseTestCaseTest extends \PHPUnit_Framework_TestCase
 {
@@ -19,24 +24,25 @@ class BaseTestCaseTest extends \PHPUnit_Framework_TestCase
 
     private $kernel;
 
+    /**
+     * @var TestTestCase
+     */
     private $testCase;
 
     private $client;
 
     protected function setUp()
     {
-        $this->container = $this->createMock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $this->container = $this->createMock(ContainerInterface::class);
         $this->container->expects($this->any())
             ->method('get')
             ->will($this->returnCallback(function ($name) {
-                $dic = [
-                    'test.client' => $this->client,
-                ];
+                $dic = ['test.client' => $this->client];
 
                 return $dic[$name];
             }));
 
-        $this->kernel = $this->createMock('Symfony\Component\HttpKernel\KernelInterface');
+        $this->kernel = $this->createMock(KernelInterface::class);
         $this->kernel->expects($this->any())
             ->method('getContainer')
             ->willReturn($this->container);
@@ -44,7 +50,7 @@ class BaseTestCaseTest extends \PHPUnit_Framework_TestCase
         $this->testCase = new TestTestCase();
         $this->testCase->setKernel($this->kernel);
 
-        $this->client = $this->createMock('Symfony\Bundle\FrameworkBundle\Client');
+        $this->client = $this->createMock(Client::class);
         $this->client->expects($this->any())
             ->method('getContainer')
             ->willReturn($this->container);
@@ -81,6 +87,9 @@ class BaseTestCaseTest extends \PHPUnit_Framework_TestCase
             'Symfony\Cmf\Component\Testing\Functional\DbManager\%s',
             $expected
         );
+        if (PHPCR::class === $className && class_exists(RepositoryManager::class)) {
+            $className = RepositoryManager::class;
+        }
 
         $this->assertInstanceOf($className, $res);
     }
